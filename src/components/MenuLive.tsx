1eccda49c417a13c0b2e18133a15d57121b0f093
AMWolfstein <ahmedhero4040@gmail.com>
2026-07-13 22:14:25 +0300

    style: redesign menu into responsive cards grid

--- src/components/MenuLive.tsx
+++ src/components/MenuLive.tsx
@@ -66,7 +66,7 @@ export default function MenuLive() {
               </span>
             </div>
 
-            <div className="grid gap-3">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
               {category.items.map((item) => (
                 <MenuItemCard key={item.id} item={item} currency={restaurant.currency} />
               ))}
